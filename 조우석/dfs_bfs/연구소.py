e2e9f4c3b0e90ffc4109d8477eea7e29d8443325
WooSeok <55547933+WooseokJ@users.noreply.github.com>
2022-05-27 11:05:05 +0900

    Update 연구소.py

--- 조우석/dfs_bfs/연구소.py
+++ 조우석/dfs_bfs/연구소.py
@@ -58,3 +58,74 @@ def dfs(count):
                 count-=1
 dfs(0)
 print(result)
+
+
+from collections import deque
+import sys
+n,m=map(int,sys.stdin.readline().split())
+graph=[]
+
+for i in range(n):
+    graph.append(list(map(int,sys.stdin.readline().split())))
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+result=0
+
+temp=[[0]*m for i in range(n)]
+
+
+def bfs():
+    global result
+    q=deque()
+    
+    #바이러스 위치 찾기 
+    for i in range(n):
+        for j in range(m):
+            temp[i][j]=graph[i][j]
+            if temp[i][j]==2:
+                q.append((i,j))
+    #바이러스가 퍼뜨리기
+    while q:
+        x,y= q.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx<=-1 or nx>=n or ny<=-1 or ny>=m:
+                continue
+            if temp[nx][ny]==0:
+                temp[nx][ny]=2
+                q.append((nx,ny)) 
+    # 0의 개수 (안전지대 개수) 카운트
+    zero_score=0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j]==0:
+                zero_score+=1
+    # 0, zero_scroe=10(0의개수)
+    result=max(result,zero_score)
+    return result
+
+# 벽을 설치하는 함수 구현
+def wall(count):
+    if count==3:
+        bfs()
+        return
+    #벽을 설치하는 과정
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j]==0:
+                graph[i][j]=1
+                count+=1
+                wall(count)
+                graph[i][j]=0
+                count-=1
+wall(0)
+print(result)
+
+    
+
+
+    
+        
+        
