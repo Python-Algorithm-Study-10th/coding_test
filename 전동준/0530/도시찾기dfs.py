62205bbf854f90c95fabb2b2dc91dbea0c2bef6c
JeonJhin <jdznawa@naver.com>
2022-06-03 05:44:19 +0900

    0603a

--- 전동준/0530/도시찾기dfs.py
+++ 전동준/0530/도시찾기dfs.py
@@ -15,15 +15,17 @@ for i in range(m):
 
 visited = [False] * (n+1)
 visited[x] = True
+distance = [[-1] for _ in range (n+1)]
+distance[x] = 0
     
-def solution(graph,start,visited):
-    idx = 0 
+def solution(graph,start,k):
+    idx = 0
     for i in graph[start]:
         if idx == visited:
             continue
-        if not graph[i]:
-            solution(graph,i,visited)
+        if not visited[i]:
+            solution(graph,i,k)
             idx += 1
-    return visited[visited]
+    return visited[k]
         
 print(solution(graph,x,k))
