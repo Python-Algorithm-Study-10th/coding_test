2751de97660f6ef07ce8750354692675a646f137
JeonJhin <jdznawa@naver.com>
2022-06-10 09:55:10 +0900

    0610

--- 전동준/0610/15_도시찾기.py
+++ 전동준/0610/15_도시찾기.py
@@ -1,4 +1,4 @@
-#도시찾기(dfs)
+#도시찾기(bfs)
 
 from collections import deque
 from sys import stdin
@@ -8,7 +8,7 @@ n,m,k,x = map(int,stdin.readline().split())
 #그래프 정의
 graph = list([] for _ in range(n+1))
 
-#도시 간의 거리 표시
+#출발 도시(x)와의 거리 표시
 distance = [-1] * (n+1)
 distance[x] = 0
 
@@ -18,22 +18,26 @@ for _ in range(m):
     graph[a].append(b)
     
 #시작 지점에서부터 거리 계산
-def solution(start,gap):
+def solution(graph1,graph2,start,goal):     #연결도 그래프, 거리 표시 그래프, 시작 지점, 목표 거리
     q = deque([start])
     while q:
         now = q.popleft()   #현재 노드 위치 조회
-        for next in graph[now]: #현재 노드와 연결된 노드 조회
-            if distance[next] == -1:    #만약 연결되어 있다는 처리가 되지 않았다면
-                distance[next] = distance[now] + 1  #연결 처리(거리 +1)
+        for next in graph1[now]: #현재 노드와 연결된 노드 조회
+            if graph2[next] == -1:    #만약 연결되어 있다는 처리가 되지 않았다면
+                graph2[next] = graph2[now] + 1  #연결 처리(거리 +1)
                 q.append(next)
 
-    #거리 k에 해당하는 도시 조회
+    #거리 goal에 해당하는 도시 조회
     check = False
+    asw = []
     for i in range(1, n+1):
-        if distance[i] == gap:
+        if graph2[i] == goal:
             check = True 
-            print(i)                
-    if check == False:  #check = True여도 None 값이 반환되는 문제 발생
-        print(-1)
+            asw.append(i)
+    if check == False:
+        return -1
+    else:
+        return asw
 
-print(solution(x,k))
+answer = solution(graph,distance,x,k)
+print(*answer,sep='\n')     #리스트로 저장된 값을 한 줄에 하나씩 반환
--- 전동준/0610/18_괄호변환.py
+++ 전동준/0610/18_괄호변환.py
@@ -1,33 +1,5 @@
 from sys import stdin
 
-def solution(p):
-    answer = ''
-    #1.빈 문자열인 경우 빈 문자열 반환
-    if len(w) == 0:
-        return answer
-    #2.균형잡힌 문자열 u,v로 분리
-    index = balanced_index(p)
-    u = p[:index+1]
-    v = p[index+1:]
-    #3.문자열 이어 붙이기
-    if checker(u):  #u가 True(균형잡힌 문자열)이면
-        answer = u + solution(v)
-    else:   #u가 False(균형이 깨진 문자열)이면
-        answer = '('
-        answer += solution(v)
-        answer += ')'
-        u = list(u[1:-1])
-        for i in range(len(u)):
-            if u[i] == '(':
-                u[i] = ')'
-            else:
-                u[i] = '('
-        answer += "".join(u)
-    return answer
-
-w = stdin.readline()
-solution(w)
-    
 #2-1.분리 기준
 def balanced_index(p):  #'('와 ')'가 짝이 맞는지 확인
     count = 0   #왼쪽 괄호 개수
@@ -50,4 +22,31 @@ def checker(p):
                 return False
             count -= 1        
     return True
-        
+
+def solution(p):
+    answer = ''
+    #1.빈 문자열인 경우 빈 문자열 반환
+    if len(p) == 0:
+        return answer
+    #2.균형잡힌 문자열 u,v로 분리
+    index = balanced_index(p)
+    u = p[:index+1]
+    v = p[index+1:]
+    #3.문자열 이어 붙이기
+    if checker(u):  #u가 True(균형잡힌 문자열)이면
+        answer = u + solution(v)
+    else:   #u가 False(균형이 깨진 문자열)이면
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += "".join(u)
+    return answer
+
+w = stdin.readline().strip()
+print(solution(w))
