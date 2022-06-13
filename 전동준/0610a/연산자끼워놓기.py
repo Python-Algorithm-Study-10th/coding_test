33f4239297c1c5496847a501d27ed791b5531296
JeonJhin <jdznawa@naver.com>
2022-06-13 10:10:56 +0900

    0613

--- 전동준/0610a/연산자끼워놓기.py
+++ 전동준/0610a/연산자끼워놓기.py
@@ -1,4 +1,5 @@
 #dfs
+#미완성
 
 from sys import stdin
 
@@ -6,24 +7,28 @@ n = map(int,stdin.readline().split())
 numlist = list(map(int,stdin.readline().split()))
 ca = list(map(int,stdin.readline().split()))
 
+maximum = -1e9
+minimum = 1e9
+
 def solution(num,depth,total,cacount):
     plus = cacount[0]
     minus = cacount[1]
     multiply = cacount[2]
     divide = cacount[3]
-    ans = []
+    global maximum,minimum
     if depth == len(num):
-        ans.append(max(total))
-        ans.append(min(total))
+        maximum = max(total,maximum)
+        minimum = min(total,minimum)
         return
     if plus > 0:
-        solution(num,depth+1, total+num[depth],[plus-1,minus,multiply,divide])
+         solution(num,depth+1, total+num[depth],[plus-1,minus,multiply,divide])
     if minus > 0:
         solution(num,depth+1, total-num[depth],[plus,minus-1,multiply,divide])
     if multiply > 0:
         solution(num,depth+1, total*num[depth],[plus,minus,multiply-1,divide])
     if divide > 0:
         solution(num,depth+1, int(total/num[depth]),[plus,minus,multiply,divide-1])
-        
-answer = solution(numlist,1,numlist[0],ca)
-print(*answer,sep='\n')
+
+solution(numlist,1,numlist[0],ca)
+print(maximum)
+print(minimum)
